481e7f002bb05188918ab4dc4497daa2d3f3feb8
Abdullah AlAnsi <aalansi@yk-bank.com>
2024-07-30 20:36:30 +0300

    add some calls and new classes

--- lib/SDK/constants.dart
+++ lib/SDK/constants.dart
@@ -1,10 +1,11 @@
 
 
-const BASE_URL = 'https://stagebas.yk-bank.com:9104';
+// const BASE_URL = 'https://stagebas.yk-bank.com:9104';
+const BASE_URL = 'https://online.ana.com.ye:4949';
 const AUTH_URL = '/conn/auth';
 const RESPONSE_TYPE="code";
 const CLIENT_ID="ykb_lite_app";
 const SCOPE="user.profile fullNameEn homeAddress idCard mail phone workInfo";
 const REDIRECT_URI=BASE_URL+"/api/v1/auth/callback";
 const CHANNEL="api";
-const CONSENT_MODE="Phone";
+const CONSENT_MODE="Face";
