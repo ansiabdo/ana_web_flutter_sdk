7a8a46b08de395b94977cf6b149cfbc935860a27
ansiabdo <ansi.abdooh@gmail.com>
2024-09-16 21:10:50 +0300

    Update constants.dart

--- lib/SDK/constants.dart
+++ lib/SDK/constants.dart
@@ -2,10 +2,10 @@
 
 // const BASE_URL = 'https://stagebas.yk-bank.com:9104';
 const BASE_URL = 'https://online.ana.com.ye:4949';
-const AUTH_URL = '/conn/auth';
+const AUTH_URL = '/web/conn/auth';
 const RESPONSE_TYPE="code";
-const CLIENT_ID="ykb_lite_app";
-const SCOPE="user.profile fullNameEn homeAddress idCard mail phone workInfo";
+const CLIENT_ID="<<CLIENT-ID>>";
+const SCOPE="<<SCOPE>>";
 const REDIRECT_URI=BASE_URL+"/api/v1/auth/callback";
-const CHANNEL="api";
-const CONSENT_MODE="Face";
+const CHANNEL="web";
+const CONSENT_MODE="none";
