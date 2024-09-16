6127b414ae4fa3256a1c229dd1351353675003e7
ansiabdo <ansi.abdooh@gmail.com>
2024-09-16 21:26:02 +0300

    Update constants.dart

--- lib/SDK/constants.dart
+++ lib/SDK/constants.dart
@@ -7,5 +7,5 @@ const RESPONSE_TYPE="code";
 const CLIENT_ID="<<CLIENT-ID>>";
 const SCOPE="<<SCOPE>>";
 const REDIRECT_URI=BASE_URL+"/api/v1/auth/callback";
-const CHANNEL="web";
+const CHANNEL="api";
 const CONSENT_MODE="none";
